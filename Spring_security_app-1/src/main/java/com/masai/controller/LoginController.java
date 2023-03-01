559e0b8f18d61930a4f637c5e3b9d37d677d187d
11-DEEPAK-KUMAR-11 <3511.deepak@gmail.com>
2023-03-01 18:51:50 +0530

    Login module

--- Spring_security_app-1/src/main/java/com/masai/controller/LoginController.java
+++ Spring_security_app-1/src/main/java/com/masai/controller/LoginController.java
@@ -1,5 +1,14 @@
 package com.masai.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LoginController {
 
+	@GetMapping("/login")
+	public String letsLogin()
+	{
+		return "Welcome to the login page";
+	}
 }
